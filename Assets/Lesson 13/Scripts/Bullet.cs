ed7f5fb9a3cd78953d1bd59e0ee580fdbaa95887
Jedi-Daddy <av.zhuravlev90@gmail.com>
2025-03-07 00:56:11 +0100

    finish lesson 14

--- Assets/Lesson 13/Scripts/Bullet.cs
+++ Assets/Lesson 13/Scripts/Bullet.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour
 {
     public float lifeTime = 5f;
     public int damageAmount = 20;
+    public int scorePerHit = 10; 
 
-    private void Star()
+    private void Start() 
     {
-        Destroy(gameObject, 10);
+        Destroy(gameObject, lifeTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(transform.GetComponent<Rigidbody>());
-        if (other.tag == "Dragon")
+
+        if (other.CompareTag("Dragon")) 
         {
             transform.parent = other.transform;
             other.GetComponent<Dragon>().TakeDamage(damageAmount);
+
+            
+            ScoreManager.instance.AddScore(scorePerHit);
         }
     }
 }
---
+++ Assets/Lesson 14/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int score = 0;
+    public Text scoreText; // Перетащи UI Text в инспекторе
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+}
