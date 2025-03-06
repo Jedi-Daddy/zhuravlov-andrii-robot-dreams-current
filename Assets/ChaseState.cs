13adb0fa3dffc7a0fdb504ab809e58cf99e3228d
Jedi-Daddy <av.zhuravlev90@gmail.com>
2025-03-06 20:14:00 +0100

    add patrol state

--- Assets/ChaseState.cs
+++ Assets/ChaseState.cs
@@ -1,41 +1,30 @@
-using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class PatrolState : StateMachineBehaviour
+
+public class ChaseState : StateMachineBehaviour
 {
-    float timer;
-    List<Transform> WayPoints = new List<Transform> ();
     NavMeshAgent agent;
-
     Transform player;
-    float chaseRange = 8;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = animator.GetComponent<NavMeshAgent>();
-        agent.speed = 1.5f;
-        timer = 0;
-        GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
-        foreach(Transform t in go.transform) 
-            WayPoints.Add(t);
-
-        agent.SetDestination(WayPoints[Random.Range(0, WayPoints.Count)].position);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent.speed = 3.5f;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (agent.remainingDistance <= agent.stoppingDistance)
-            agent.SetDestination(WayPoints[Random.Range(0, WayPoints.Count)].position);
-        timer += Time.deltaTime;
-        if (timer > 10)
-            animator.SetBool("IsPatrolling", false);
+        agent.SetDestination(player.position);
         float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance < chaseRange)
-            animator.SetBool("IsChasing", true);
+        if (distance < 15)
+            animator.SetBool("IsChasing", false);
+        if (distance < 2.5f)
+            animator.SetBool("IsAttacking", true);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
